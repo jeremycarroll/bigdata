d4a452c73912eb918833512fb6c97adffdab095b
thompsonbry <thompsonbry@8f7bc0f5-282e-0410-95e3-8d296e9bb460>
2009-08-04 16:22:17 +0000

    modified to recognize and process RDF formats which have been compressed if their file name ends with .zip or .gz.
    git-svn-id: https://svn.code.sf.net/p/bigdata/code/branches/BIGDATA_RDF_DATA_LOADER_BRANCH@1996 8f7bc0f5-282e-0410-95e3-8d296e9bb460

--- bigdata-rdf/src/java/com/bigdata/rdf/load/RDFFilenameFilter.java
+++ bigdata-rdf/src/java/com/bigdata/rdf/load/RDFFilenameFilter.java
@@ -7,11 +7,13 @@ import java.io.Serializable;
 import org.openrdf.rio.RDFFormat;
 
 /**
- * Filter recognizes anything that is a registered as an {@link RDFFormat}.
- * If it does not recognize your format, then you can create a subclass
- * which ensures the static initialization of your format with
- * {@link RDFFormat}. That needs to be done in static code so that it will
- * be performed on the machine where this filter is being used.
+ * Filter recognizes anything that is a registered as an {@link RDFFormat} or
+ * which would be recognized as such if you stripped off a trailing
+ * <code>.zip</code> or <code>.gz</code> filename extension. If it does not
+ * recognize your format, then you can create a subclass which ensures the
+ * static initialization of your format with {@link RDFFormat}. That needs to
+ * be done in static code so that it will be performed on the machine where this
+ * filter is being used.
  */
 public class RDFFilenameFilter implements FilenameFilter, Serializable {
 
@@ -20,7 +22,7 @@ public class RDFFilenameFilter implements FilenameFilter, Serializable {
      */
     private static final long serialVersionUID = -628798437502907063L;
 
-    public boolean accept(File dir, String name) {
+    public boolean accept(final File dir, final String name) {
 
         final File file = new File(dir, name);
         
@@ -37,8 +39,28 @@ public class RDFFilenameFilter implements FilenameFilter, Serializable {
 
         }
 
-        // if recognizable as RDF.
-        return RDFFormat.forFileName(name) != null;
+        if (RDFFormat.forFileName(name) != null) {
+            // recognizable as RDF.
+            return true;
+        }
+        
+        if (name.endsWith(".gz")) {
+            final String s = name.substring(0, name.length() - 3);
+            if (RDFFormat.forFileName(s) != null) {
+                // recognizable as gzip'd RDF.
+                return true;
+            }
+        }
+        
+        if (name.endsWith(".zip")) {
+            final String s = name.substring(0, name.length() - 4);
+            if (RDFFormat.forFileName(s) != null) {
+                // recognizable as gzip'd RDF.
+                return true;
+            }
+        }
+        
+        return false;
 
     }
 
