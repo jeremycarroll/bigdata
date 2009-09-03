938d11da51bc696dea708c7618eb7d06290445b3
thompsonbry <thompsonbry@8f7bc0f5-282e-0410-95e3-8d296e9bb460>
2009-09-03 15:46:06 +0000

    Fixed bug where a Temporary BufferMode, which is backed by disk, would not be extended beyond 2G (Integer#MAX_VALUE).
    Also fixed a bug where a request larger than the initial extent could result in an extension of the store smaller than the required amount.
    
    Added BufferMode#isFullyBuffered() to exactly capture the semantics of a store with an image of its contents in memory, which is what motivates the upper bound of 2G on a Transient store (but not on a Temporary store).
    
    git-svn-id: https://svn.code.sf.net/p/bigdata/code/branches@2046 8f7bc0f5-282e-0410-95e3-8d296e9bb460

--- BTREE_BUFFER_BRANCH/bigdata/src/java/com/bigdata/journal/AbstractBufferStrategy.java
+++ BTREE_BUFFER_BRANCH/bigdata/src/java/com/bigdata/journal/AbstractBufferStrategy.java
@@ -97,7 +97,7 @@ public abstract class AbstractBufferStrategy extends AbstractRawWormStore implem
      * array or native memory (both are limited to int32 bytes since they
      * are addressed by a Java <code>int</code>).
      */
-    protected static final String ERR_INT32 = "Would exceed int32 bytes.";
+    protected static final String ERR_INT32 = "Would exceed int32 bytes (not allowed unless backed by disk).";
     
     /**
      * Text of the error message used when
@@ -110,6 +110,14 @@ public abstract class AbstractBufferStrategy extends AbstractRawWormStore implem
      * Error message used when the writes are not allowed.
      */
     protected static final String ERR_READ_ONLY = "Read only";
+
+    /**
+     * Error message used when the record size is invalid (e.g., negative).
+     * 
+     * @todo There is some overlap with {@link #ERR_RECORD_LENGTH_ZERO} and
+     *       {@link #ERR_BUFFER_EMPTY}.
+     */
+    protected static final String ERR_BAD_RECORD_SIZE = "Bad record size";
     
     /**
      * Error message used when the store is closed. 
@@ -277,22 +285,24 @@ public abstract class AbstractBufferStrategy extends AbstractRawWormStore implem
     /**
      * Invoked if the store would exceed its current extent by
      * {@link #write(ByteBuffer)}. The default behavior extends the capacity of
-     * the buffer by the maximum of 32M or the {@link Options#INITIAL_EXTENT}.
+     * the buffer by the at least the requested amount and a maximum of 32M or
+     * the {@link Options#INITIAL_EXTENT}.
+     * <p>
+     * If the data are fully buffered, then the maximum store size is limited to
+     * int32 bytes which is the maximum #of bytes that can be addressed in RAM
+     * (the pragmatic maximum is slightly less than 2G due to the limits of the
+     * JVM to address system memory).
      * 
      * @return true if the capacity of the store was extended and the write
-     *         operation should be retried. If the data are fully buffered, the
-     *         the maximum store size is limited to int32 bytes which is the
-     *         maximum #of bytes that can be addressed in RAM (the pragmatic
-     *         maximum is slightly less than 2G due to the limits of the JVM to
-     *         address system memory).
+     *         operation should be retried.
      */
-    final public boolean overflow(long needed) {
+    final public boolean overflow(final long needed) {
 
         final long userExtent = getUserExtent();
         
         final long required = userExtent + needed;
         
-        if (required > Integer.MAX_VALUE && bufferMode != BufferMode.Disk) {
+        if (required > Integer.MAX_VALUE && bufferMode.isFullyBuffered()) {
             
             /*
              * Would overflow int32 bytes and data are buffered in RAM.
@@ -303,9 +313,9 @@ public abstract class AbstractBufferStrategy extends AbstractRawWormStore implem
             return false;
             
         }
-        
-        if( maximumExtent != 0L && required > maximumExtent ) {
-            
+
+        if (maximumExtent != 0L && required > maximumExtent) {
+
             /*
              * Would exceed the maximum extent (iff a hard limit).
              * 
@@ -313,32 +323,46 @@ public abstract class AbstractBufferStrategy extends AbstractRawWormStore implem
              * overflows the in-memory buffer onto the disk.
              */
 
-            if(WARN)
-                log.warn("Would exceed maximumExtent="+maximumExtent);
+            if (WARN)
+                log.warn("Would exceed maximumExtent=" + maximumExtent);
 
             return false;
-            
+
         }
-        
+
         /*
-         * Increase by the initial extent or by 32M, whichever is greater.
+         * Increase by the initial extent or by 32M, whichever is greater, but
+         * by no less that the requested amount.
          */
         long newExtent = userExtent
-                + Math.max(initialExtent, Bytes.megabyte * 32);
+                + Math.max(needed, Math.max(initialExtent,
+                                Bytes.megabyte * 32));
         
-        if( newExtent > Integer.MAX_VALUE && bufferMode != BufferMode.Disk) {
+        if (newExtent > Integer.MAX_VALUE && bufferMode.isFullyBuffered()) {
 
             /*
-             * Do not allocate more than int32 bytes when using a buffered mode.
+             * Do not allocate more than int32 bytes when using a buffered mode
+             * (anything not backed by the disk).
              */
+
             newExtent = Integer.MAX_VALUE;
+
+            if (newExtent - userExtent < needed) {
+
+                /*
+                 * Not enough room in memory for the requested extension.
+                 */
+
+                log.error(ERR_INT32);
+                
+            }
             
         }
 
         /*
          * Extend the capacity.
          */
-        truncate( newExtent );
+        truncate(newExtent);
 
         // report event.
         ResourceManager.extendJournal(getFile() == null ? null : getFile()
