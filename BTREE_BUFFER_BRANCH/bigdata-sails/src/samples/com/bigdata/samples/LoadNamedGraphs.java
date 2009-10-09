a98f797292372d4c92e40cfa699a59ce2d9dce42
thompsonbry <thompsonbry@8f7bc0f5-282e-0410-95e3-8d296e9bb460>
2009-10-09 18:51:55 +0000

    modified (hacked) harness to load from .gz files in a directory
    also modified the harness to take the journal filename from the properties file, which is really where it should be specified.
    
    git-svn-id: https://svn.code.sf.net/p/bigdata/code/branches@2181 8f7bc0f5-282e-0410-95e3-8d296e9bb460

--- BTREE_BUFFER_BRANCH/bigdata-sails/src/samples/com/bigdata/samples/LoadNamedGraphs.java
+++ BTREE_BUFFER_BRANCH/bigdata-sails/src/samples/com/bigdata/samples/LoadNamedGraphs.java
@@ -32,10 +32,13 @@ import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
 import java.util.Properties;
+import java.util.zip.GZIPInputStream;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
 
@@ -43,7 +46,7 @@ import org.openrdf.repository.Repository;
 import org.openrdf.repository.RepositoryConnection;
 import org.openrdf.rio.RDFFormat;
 
-import com.bigdata.btree.IndexMetadata;
+import com.bigdata.rdf.load.RDFFilenameFilter;
 import com.bigdata.rdf.sail.BigdataSail;
 import com.bigdata.rdf.sail.BigdataSailRepository;
 
@@ -60,30 +63,32 @@ public class LoadNamedGraphs extends SampleCode {
      * 
      * @throws Exception
      */
-    public void loadAll(final File file) throws Exception {
+    public void loadAll(final Properties properties, final File file) throws Exception {
 
-        /*
-         * We are going to use the "quads" mode. Right now, the quads mode does
-         * not do inference AT ALL.
-         */
-        final Properties properties = loadProperties("quads.properties");
+		/*
+		 * We are going to use the "quads" mode. Right now, the quads mode does
+		 * not do inference AT ALL.
+		 */
+//		final File propertyFile = new File(
+//				"c:/bigdata-data-sets/LoadNamedGraphs.properties");
+    	
+//        // create a backing file
+//        final File journalFile = new File("c:/bigdata.jnl");
+////        final File journalFile = File.createTempFile("bigdata", ".jnl");
+////        journalFile.deleteOnExit();
+//        properties.setProperty(BigdataSail.Options.FILE, journalFile
+//                .getAbsolutePath());
 
-        // create a backing file
-        final File journal = new File("d:/bigdata.jnl");
-//        final File journal = File.createTempFile("bigdata", ".jnl");
-//        journal.deleteOnExit();
-        properties.setProperty(BigdataSail.Options.FILE, journal
-                .getAbsolutePath());
-
-        /*
-         * Override the write retention queue (default is 500).
-         * 
-         * This makes a BIG difference in the journal size and throughput if you
-         * are bulk loading data and have enough RAM.
-         */
-        properties.setProperty(
-                IndexMetadata.Options.WRITE_RETENTION_QUEUE_CAPACITY, "8000");
-
+        // You can do the overrides in the property file.
+//        /*
+//         * Override the write retention queue (default is 500).
+//         * 
+//         * This makes a BIG difference in the journal size and throughput if you
+//         * are bulk loading data and have enough RAM.
+//         */
+//        properties.setProperty(
+//                IndexMetadata.Options.WRITE_RETENTION_QUEUE_CAPACITY, "8000");
+//
 //        properties.setProperty(IndexMetadata.Options.BTREE_BRANCHING_FACTOR,
 //                "64");
 
@@ -98,41 +103,118 @@ public class LoadNamedGraphs extends SampleCode {
             final long stmtsBefore = cxn.size();
             final long start = System.currentTimeMillis();
 
-            // then process the sample data files one at a time
-            InputStream is = new FileInputStream(file);
-            ZipInputStream zis = new ZipInputStream(new BufferedInputStream(is));
-            ZipEntry ze = null;
-            while ((ze = zis.getNextEntry()) != null) {
-                if (ze.isDirectory()) {
-                    continue;
-                }
-                String name = ze.getName();
-                log.info(name);
-                ByteArrayOutputStream baos = new ByteArrayOutputStream();
-                byte[] bytes = new byte[4096];
-                int count;
-                while ((count = zis.read(bytes, 0, 4096)) != -1) {
-                    baos.write(bytes, 0, count);
-                }
-                baos.close();
-                final Reader reader = new InputStreamReader(
-                        new ByteArrayInputStream(baos.toByteArray()));
-                String baseIRI = file.toURI()+"/"+name;
-                
-                cxn.add(reader, baseIRI, RDFFormat.forFileName(name));
+            if (file.getName().endsWith(".zip")
+					|| file.getName().endsWith(".ZIP")) {
 
-                // note: due to buffering, this reports stmts flush to the db
-                // not stmts added to the cxn.
-                long elapsed = System.currentTimeMillis() - start;
-                long stmtsAfter = cxn.size();
-                long stmtsAdded = stmtsAfter - stmtsBefore;
-                int throughput = (int) ((double) stmtsAdded / (double) elapsed * 1000d);
-                System.err.println("loaded: " + name + " : " + stmtsAdded
-                        + " stmts in " + elapsed + " millis: " + throughput
-                        + " stmts/sec");
-            }
-            zis.close();
+				// then process the sample data files one at a time
+				InputStream is = new FileInputStream(file);
+				ZipInputStream zis = new ZipInputStream(
+						new BufferedInputStream(is));
+				ZipEntry ze = null;
+				while ((ze = zis.getNextEntry()) != null) {
+					if (ze.isDirectory()) {
+						continue;
+					}
+					String name = ze.getName();
+					if (log.isInfoEnabled())
+						log.info(name);
+					ByteArrayOutputStream baos = new ByteArrayOutputStream();
+					byte[] bytes = new byte[4096];
+					int count;
+					while ((count = zis.read(bytes, 0, 4096)) != -1) {
+						baos.write(bytes, 0, count);
+					}
+					baos.close();
+					final Reader reader = new InputStreamReader(
+							new ByteArrayInputStream(baos.toByteArray()));
+					String baseIRI = file.toURI() + "/" + name;
 
+					cxn.add(reader, baseIRI, RDFFormat.forFileName(name));
+
+					// note: due to buffering, this reports stmts flush to the
+					// db
+					// not stmts added to the cxn.
+					long elapsed = System.currentTimeMillis() - start;
+					long stmtsAfter = cxn.size();
+					long stmtsAdded = stmtsAfter - stmtsBefore;
+					int throughput = (int) ((double) stmtsAdded
+							/ (double) elapsed * 1000d);
+					System.err.println("loaded: " + name + " : " + stmtsAdded
+							+ " stmts in " + elapsed + " millis: " + throughput
+							+ " stmts/sec");
+				}
+				zis.close();
+				
+			} else if(file.isDirectory()) {
+				
+				final File[] files = file.listFiles(new RDFFilenameFilter());
+				
+				if (files != null) {
+				
+					for (File f : files) {
+
+						System.err.println("Reading: " + f);
+						
+						final Reader reader = new InputStreamReader(
+								(f.getName().endsWith(".gz")
+										|| f.getName().endsWith(".GZ") ? new GZIPInputStream(
+										new FileInputStream(f))
+										: new FileInputStream(f)));
+
+						final String baseIRI = file.toURI().toString();
+
+						cxn.add(reader, baseIRI, RDFFormat.forFileName(f
+								.getName()));
+
+						// note: due to buffering, this reports stmts flush to the
+						// db
+						// not stmts added to the cxn.
+						long elapsed = System.currentTimeMillis() - start;
+						long stmtsAfter = cxn.size();
+						long stmtsAdded = stmtsAfter - stmtsBefore;
+						int throughput = (int) ((double) stmtsAdded
+								/ (double) elapsed * 1000d);
+
+						System.err.println("loaded: " + f + " : " + stmtsAdded
+								+ " stmts in " + elapsed + " millis: " + throughput
+								+ " stmts/sec");
+
+						reader.close();
+						
+					}
+					
+				}
+				
+			} else if(file.isFile()) {
+				
+				final Reader reader = new InputStreamReader(
+						new FileInputStream(file));
+
+				final String baseIRI = file.toURI().toString();
+
+				cxn.add(reader, baseIRI, RDFFormat.forFileName(file
+						.getName()));
+
+				// note: due to buffering, this reports stmts flush to the
+				// db not stmts added to the cxn.
+				long elapsed = System.currentTimeMillis() - start;
+				long stmtsAfter = cxn.size();
+				long stmtsAdded = stmtsAfter - stmtsBefore;
+				int throughput = (int) ((double) stmtsAdded
+						/ (double) elapsed * 1000d);
+
+				System.err.println("loaded: " + file + " : " + stmtsAdded
+						+ " stmts in " + elapsed + " millis: " + throughput
+						+ " stmts/sec");
+
+				reader.close();
+				
+			} else {
+				
+				System.err.println("Can not load: "+file);
+				
+			}
+            
             // autocommit is false, we need to commit our SAIL "transaction"
             cxn.commit();
 
@@ -141,8 +223,8 @@ public class LoadNamedGraphs extends SampleCode {
             long stmtsAfter = cxn.size();
             long stmtsAdded = stmtsAfter - stmtsBefore;
             int throughput = (int) ((double) stmtsAdded / (double) elapsed * 1000d);
-            log.info("statements after: " + stmtsAfter);
-            log.info("loaded: " + stmtsAdded + " in " + elapsed + " millis: "
+            System.err.println("statements after: " + stmtsAfter);
+            System.err.println("loaded: " + stmtsAdded + " in " + elapsed + " millis: "
                     + throughput + " stmts/sec");
 
         } catch (Exception ex) {
@@ -151,31 +233,72 @@ public class LoadNamedGraphs extends SampleCode {
         } finally {
             // close the repository connection
             cxn.close();
+            sail.shutDown();
         }
 
     }
 
-    /**
-     * Loads a bunch of data from a zip file.
-     * 
-     * @param args
-     *            The file name.
-     * 
-     * @throws Exception
-     */
-    public static void main(String[] args) {
+	/**
+	 * Loads a bunch of data from a file, zip file, or directory
+	 * (non-recursive). You can use <code>quad.properties</code> as the
+	 * properties file or anything else that you like.
+	 * 
+	 * @param args
+	 *            The name of the property and the name of the file or directory
+	 *            to load.
+	 * 
+	 * @throws Exception
+	 */
+    public static void main(final String[] args) {
 
-        if (args.length == 0) {
+        if (args.length < 2 ) {
 
-            System.out.println("usage: file.zip");
+            System.out.println("usage: properties fileOrDirectoryOrZip");
 
             System.exit(1);
 
         }
 
+		final Properties properties;
+		try {
+
+			final File propertyFile = new File(args[0]);
+
+			if (!propertyFile.exists()) {
+
+				throw new FileNotFoundException(propertyFile.toString());
+				
+			}
+
+			properties = new Properties();
+			
+			final InputStream is = new BufferedInputStream(new FileInputStream(
+					propertyFile));
+			
+			try {
+		
+				properties.load(is);
+				
+			} finally {
+				
+				is.close();
+				
+			}
+
+		} catch(IOException ex) {
+			
+			throw new RuntimeException(ex);
+			
+		}
+    	
         try {
        
-            new LoadNamedGraphs().loadAll(new File(args[0]));
+        	final File dataFileOrDirectory = new File(args[1]);
+        	
+        	if (!dataFileOrDirectory.exists())
+				throw new FileNotFoundException(dataFileOrDirectory.toString());
+        	
+            new LoadNamedGraphs().loadAll(properties, dataFileOrDirectory);
             
         } catch (Exception ex) {
             
