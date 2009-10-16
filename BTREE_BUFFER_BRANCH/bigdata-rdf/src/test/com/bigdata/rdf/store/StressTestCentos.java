14fd3042e480c27f3e9acce0af22ba05168a9232
thompsonbry <thompsonbry@8f7bc0f5-282e-0410-95e3-8d296e9bb460>
2009-10-16 16:23:08 +0000

    config update for scale-out, javadoc edits for LRUNexus
    git-svn-id: https://svn.code.sf.net/p/bigdata/code/branches@2217 8f7bc0f5-282e-0410-95e3-8d296e9bb460

--- BTREE_BUFFER_BRANCH/bigdata-rdf/src/test/com/bigdata/rdf/store/StressTestCentos.java
+++ BTREE_BUFFER_BRANCH/bigdata-rdf/src/test/com/bigdata/rdf/store/StressTestCentos.java
@@ -9,12 +9,17 @@ import com.bigdata.rdf.model.BigdataURI;
 import com.bigdata.rdf.model.StatementEnum;
 import com.bigdata.rdf.rio.StatementBuffer;
 import com.bigdata.rdf.vocab.NoVocabulary;
+import com.bigdata.relation.accesspath.BlockingBuffer;
 
 /**
  * This is a test class that fails on centos 64bit. On working machines, it runs
  * and always prints out 100, but on centos it will randomly print out 0. Note
  * that the test as written is specific to a quad store (it makes the quad store
- * assumption about the context position).
+ * assumption about the context position). (This issue has been linked to a bug
+ * in {@link BlockingBuffer#add(Object)}, where it was failing to test the
+ * return code from
+ * {@link BlockingBuffer#add(Object, long, java.util.concurrent.TimeUnit)} and
+ * the latter had a bug in its timeout logic.)
  * 
  * @author <a href="mailto:thompsonbry@users.sourceforge.net">Bryan Thompson</a>
  * @version $Id$
@@ -29,7 +34,7 @@ public class StressTestCentos extends AbstractTripleStoreTestCase {
         properties.setProperty(AbstractTripleStore.Options.AXIOMS_CLASS, NoAxioms.class.getName());
         properties.setProperty(AbstractTripleStore.Options.STORE_BLANK_NODES, "true");
         properties.setProperty(AbstractTripleStore.Options.JUSTIFY, "false");
-        properties.setProperty(AbstractTripleStore.Options.QUADS, "true");
+//        properties.setProperty(AbstractTripleStore.Options.QUADS, "true");
         properties.setProperty(AbstractTripleStore.Options.BLOOM_FILTER, "false");
         properties.setProperty(AbstractTripleStore.Options.VOCABULARY_CLASS, NoVocabulary.class.getName());
         properties.setProperty(AbstractTripleStore.Options.NESTED_SUBQUERY, "false");
