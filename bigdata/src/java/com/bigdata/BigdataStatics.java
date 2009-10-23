8ce438e6874450e9fed367b0a044e6ad7598cc1a
thompsonbry <thompsonbry@8f7bc0f5-282e-0410-95e3-8d296e9bb460>
2009-10-23 15:16:32 +0000

    trying again to turn on svn auto-props locally
    git-svn-id: https://svn.code.sf.net/p/bigdata/code/branches/BTREE_BUFFER_BRANCH@2251 8f7bc0f5-282e-0410-95e3-8d296e9bb460

--- bigdata/src/java/com/bigdata/BigdataStatics.java
+++ bigdata/src/java/com/bigdata/BigdataStatics.java
@@ -32,7 +32,7 @@ import com.bigdata.jini.start.process.ProcessHelper;
 /**
  * A class for those few statics that it makes sense to reference from other
  * places. This is also used to indicate the SVN revision of the source code for
- * a build. Trying again.
+ * a build. Trying again and again.
  * 
  * @author <a href="mailto:thompsonbry@users.sourceforge.net">Bryan Thompson</a>
  * @version $Id$
