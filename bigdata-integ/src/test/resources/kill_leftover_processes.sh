dd0e0d4c4329f92815e47b52def847a0450d7f6b
blevine218 <blevine218@8f7bc0f5-282e-0410-95e3-8d296e9bb460>
2010-09-27 20:01:43 +0000

    minor comment update
    git-svn-id: https://svn.code.sf.net/p/bigdata/code/branches/maven_scaleout@3641 8f7bc0f5-282e-0410-95e3-8d296e9bb460

--- bigdata-integ/src/test/resources/kill_leftover_processes.sh
+++ bigdata-integ/src/test/resources/kill_leftover_processes.sh
@@ -1,2 +1,7 @@
 #! /bin/bash
+#
+# Kill all instances of the java QuorumPeerMain process
+# TODO: Make this more generic so that we can kill processes
+#       with other names as well
+#
 kill `jps | grep QuorumPeerMain | cut -d' ' -f1 `
