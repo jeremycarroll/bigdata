d2a303d85a76d5e53d31650fa18d0f7bf5cac5d2
thompsonbry <thompsonbry@8f7bc0f5-282e-0410-95e3-8d296e9bb460>
2011-09-21 19:27:13 +0000

    A variety of license / copyright notice cleanup related changes.
    done. Added com.bigdata.Depends.  This file has the name of each dependency component, the project URL, and the license URL.  This can be used to self-report on the dependencies.  Not all dependencies are required for all deployments, but this class does not presently allow selective reporting based on the kind of deployment (standalone versus cluster).
    
    done. Modified the Banner class to include a notice for each dependency, including the name of the component, the project URL, and a link to the license file.
    
    done. Our own license file is not making it into the top-level of bigdata.jar or bigdata.war. The JAR now includes the NOTICE files that we have in our source tree for the code that is bundled into the JAR. The JAR does not bundle those dependencies so it does not need to include the LEGAL/* files for them dependencies. However, we DO need the LICENSE files for Apache and Sesame since we have imported some code from those projects. The WAR actually redistributes dependencies. It needs to include all the files in the LEGAL directories in our source tree (these are the dependency license files) plus all the NOTICE files in those dependencies.  Wrote an ant task to concatenate all NOTICE(.txt) files into a single combined NOTICE file. The JAR now includes the combined NOTICE for the bigdata source tree as bundled into the JAR.  The WAR now includes the combined NOTICE for all dependencies (even those which are not actually bundled into the
    WAR).
    
    done. The overview.html file is missing for the javadoc.
    
    done. Verify WAR deployment.
    
    done. Bump up the branching factor for the SPO relation for the default kb when deploying under the webapp.
    
    
    git-svn-id: https://svn.code.sf.net/p/bigdata/code/branches/BIGDATA_RELEASE_1_0_0@5230 8f7bc0f5-282e-0410-95e3-8d296e9bb460

--- bigdata/src/java/com/bigdata/Banner.java
+++ bigdata/src/java/com/bigdata/Banner.java
@@ -28,14 +28,22 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 package com.bigdata;
 
+import java.lang.reflect.Field;
 import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+import java.util.Collections;
 import java.util.Date;
+import java.util.Formatter;
+import java.util.LinkedHashMap;
+import java.util.Map;
 import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicReference;
 
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 import org.apache.system.SystemUtil;
 
+import com.bigdata.Depends.Dependency;
 import com.bigdata.counters.AbstractStatisticsCollector;
 import com.bigdata.util.InnerCause;
 
@@ -48,6 +56,11 @@ import com.bigdata.util.InnerCause;
  * @version $Id$
  */
 public class Banner {
+
+    /**
+     * The logger for <em>this</em> class.
+     */
+    private static final Logger log = Logger.getLogger("com.bigdata.Banner");
 
     private final static AtomicBoolean didBanner = new AtomicBoolean(false);
 
@@ -72,7 +85,7 @@ public class Banner {
 
     }
     
-    synchronized static public void banner() {
+    static public void banner() {
         
         if(didBanner.compareAndSet(false/*expect*/, true/*update*/)) {
         
@@ -80,54 +93,54 @@ public class Banner {
 
             if (!quiet) {
 
-                System.out.println(banner);
-
-            }
+                final StringBuilder sb = new StringBuilder(banner);
          
-            final Logger log = Logger.getLogger("com.bigdata");
-
-            if (log.getLevel() == null) {
-
-                /*
-                 * Since there is no default for com.bigdata, default to WARN.
-                 */
-                try {
-
-                    log.setLevel(Level.WARN);
-
-                    if (!quiet)
-                        log.warn("Defaulting log level to WARN: "
-                                + log.getName());
-
-                } catch (Throwable t) {
-                
-                    /*
-                     * Note: The SLF4J bridge can cause a NoSuchMethodException
-                     * to be thrown out of Logger.setLevel(). We trap this
-                     * exception and log a message @ ERROR. It is critical that
-                     * bigdata logging is properly configured as logging at INFO
-                     * for com.bigdata will cause a tremendous loss of
-                     * performance.
-                     * 
-                     * @see https://sourceforge.net/apps/trac/bigdata/ticket/362
-                     */
-                    if (InnerCause.isInnerCause(t, NoSuchMethodException.class)) {
-
-                        log.error("Unable to raise the default log level to WARN."
-                                + " Logging is NOT properly configured."
-                                + " Severe performance penalty will result.");
-
-                    } else {
-                        
-                        // Something else that we are not expecting.
-                        throw new RuntimeException(t);
-                        
+                // Add in the dependencies.
+                {
+                    int maxNameLen = 0, maxProjectLen = 0, maxLicenseLen = 0;
+                    for (Dependency dep : com.bigdata.Depends.depends()) {
+                        if (dep.getName().length() > maxNameLen)
+                            maxNameLen = dep.getName().length();
+                        if (dep.projectURL().length() > maxProjectLen)
+                            maxProjectLen = dep.projectURL().length();
+                        if (dep.licenseURL().length() > maxLicenseLen)
+                            maxLicenseLen = dep.licenseURL().length();
                     }
+                    maxNameLen = Math.min(80, maxNameLen);
+                    maxProjectLen = Math.min(80, maxProjectLen);
+                    maxLicenseLen = Math.min(80, maxLicenseLen);
+
+                    final Formatter f = new Formatter(sb);
+
+                    final String fmt1 = "" //
+                            + "%-" + maxNameLen + "s"//
+//                            + " %-" + maxProjectLen + "s" //
+                            + " %-" + maxLicenseLen + "s"//
+                            + "\n";
+
+                    f.format(fmt1, "Dependency", "License");
                     
+                    for (Dependency dep : com.bigdata.Depends.depends()) {
+
+                        f.format(fmt1, //
+                                dep.getName(),// 
+//                                dep.projectURL(),//
+                                dep.licenseURL()//
+                                );
+
+                    }
                 }
+                
+                System.out.println(sb);
+                
+            }
 
-            } // if(log.getLevel() == null)
-
+            /*
+             * If logging is not configured for [com.bigdata] then we set a
+             * default log level @ WARN.  This is critical for good performance.
+             */
+            setDefaultLogLevel(quiet);
+            
             /*
              * Note: I have modified this to test for disabled registration and
              * to use reflection in order to decouple the JMX dependency for
@@ -159,7 +172,159 @@ public class Banner {
         }
         
     }
+    
+    /**
+     * If logging is not configured for [com.bigdata] then we set a default log
+     * level @ WARN. This is critical for good performance.
+     */
+    private static void setDefaultLogLevel(final boolean quiet) {
 
+        final Logger defaultLog = Logger.getLogger("com.bigdata");
+
+        if (defaultLog.getLevel() == null) {
+
+            /*
+             * Since there is no default for com.bigdata, default to WARN.
+             */
+            try {
+
+                defaultLog.setLevel(Level.WARN);
+
+                if (!quiet)
+                    log.warn("Defaulting log level to WARN: "
+                            + defaultLog.getName());
+
+            } catch (Throwable t) {
+
+                /*
+                 * Note: The SLF4J bridge can cause a NoSuchMethodException to
+                 * be thrown out of Logger.setLevel(). We trap this exception
+                 * and log a message @ ERROR. It is critical that bigdata
+                 * logging is properly configured as logging at INFO for
+                 * com.bigdata will cause a tremendous loss of performance.
+                 * 
+                 * @see https://sourceforge.net/apps/trac/bigdata/ticket/362
+                 */
+                if (InnerCause.isInnerCause(t, NoSuchMethodException.class)) {
+
+                    log.error("Unable to raise the default log level to WARN."
+                            + " Logging is NOT properly configured."
+                            + " Severe performance penalty will result.");
+
+                } else {
+
+                    // Something else that we are not expecting.
+                    throw new RuntimeException(t);
+
+                }
+
+            }
+
+        } // if(log.getLevel() == null)
+
+    }
+
+    /**
+     * Use reflection to discover and report on the bigdata build information. A
+     * <code>com.bigdata.BuildInfo</code> is built when the JAR is created.
+     * However, it may not be present when running under an IDE from the source
+     * code and, therefore, there MUST NOT be any compile time references to the
+     * <code>com.bigdata.BuildInfo</code> class. This method uses reflection to
+     * avoid a compile time dependency.
+     * <p>
+     * Note: This method works fine. However, the problem with exposing the
+     * information is that people running from an IDE can observe <em>stale</em>
+     * data from old <code>com.bigdata.BuildInfo</code> class files left from a
+     * previous build of a JAR. This makes the information good for deployed
+     * versions of the JAR but potentially misleading when people are running
+     * under an IDE.
+     * 
+     * @return Build info metadata iff available.
+     */
+    private synchronized static Map<String,String> getBuildInfo() {
+
+        if (buildInfoRef.get() == null) {
+
+            final Map<String,String> map = new LinkedHashMap<String, String>();
+            
+            try {
+            
+                final Class<?> cls = Class.forName("com.bigdata.BuildInfo");
+                
+                for (Field f : cls.getFields()) {
+
+                    final String name = f.getName();
+
+                    final int mod = f.getModifiers();
+
+                    if (!Modifier.isStatic(mod))
+                        continue;
+
+                    if (!Modifier.isPublic(mod))
+                        continue;
+
+                    if (!Modifier.isFinal(mod))
+                        continue;
+
+                    try {
+
+                        final Object obj = f.get(null/* staticField */);
+
+                        if (obj != null) {
+
+                            map.put(name, "" + obj);
+                            
+                        }
+                        
+                    } catch (IllegalArgumentException e) {
+
+                        log.warn("Field: " + name + " : " + e);
+                        
+                    } catch (IllegalAccessException e) {
+                        
+                        log.warn("Field: " + name + " : " + e);
+                        
+                    }
+
+                }
+
+            } catch (ClassNotFoundException e) {
+
+                log.warn("Not found: " + "com.bigdata.BuildInfo");
+
+            } catch (Throwable t) {
+
+                log.error(t, t);
+
+            }
+
+            // set at most once.
+            buildInfoRef.compareAndSet(null/* expect */,
+                    Collections.unmodifiableMap(map)/* update */);
+
+        }
+        
+        return buildInfoRef.get();
+        
+    }
+
+    private final static AtomicReference<Map<String, String>> buildInfoRef = new AtomicReference<Map<String, String>>();
+    
+    private final static String getBuildString() {
+
+        if (getBuildInfo().isEmpty())
+            return "";
+
+        final StringBuilder s = new StringBuilder();
+
+        s.append("\nbuildVersion=" + getBuildInfo().get("buildVersion"));
+
+//        s.append("\nsvnRevision =" + getBuildInfo().get("svnRevision"));
+        
+        return s.toString();
+
+    }
+    
     /**
      * Outputs the banner and exits.
      * 
@@ -168,7 +333,7 @@ public class Banner {
      */
     public static void main(final String[] args) {
         
-        System.out.println(banner);
+        banner();
 
     }
     
@@ -188,7 +353,8 @@ public class Banner {
                 + " " + SystemUtil.architecture() + //
         "\n"+SystemUtil.cpuInfo() + " #CPU="+SystemUtil.numProcessors() +//
         "\n"+System.getProperty("java.vendor")+" "+System.getProperty("java.version")+
-        "\n"
+        getBuildString()+ // Note: Will add its own newline if non-empty.
+        "\n\n"
         ;
     
 }
